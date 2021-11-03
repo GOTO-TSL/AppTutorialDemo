85c789359892ba2584c3c830749126e9e3258e57
GOTO-TSL <kosuke.iosappbuilding@gmail.com>
2021-11-03 15:12:43 +0900

    Initial Commit

---
+++ AppTutorialDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppTutorialDemo
+//
+//  Created by 後藤孝輔 on 2021/11/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
